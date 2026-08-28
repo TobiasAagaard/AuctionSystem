2bb5b9077667abc81d865ee728edf4c262d3ec2b
René <reneskjodt333@gmail.com>
2026-08-28 13:24:14 +0200

    Add BusinessCustomer and PrivateCustomer model classes

---
+++ Auction.Core/Models/BusinessCustomer.cs
@@ -0,0 +1,14 @@
+﻿namespace Auction.Core.Models;
+
+public class BusinessCustomer : User
+{
+    public BusinessCustomer(int id, string username, string passwordHash, int postalCode, decimal credit, string cvr)
+        : base(id, username, passwordHash, postalCode)
+    {
+        Credit = credit;
+        CVR = cvr;
+    }
+
+    public decimal Credit { get; set;}
+    public string CVR { get; set; }
+}
