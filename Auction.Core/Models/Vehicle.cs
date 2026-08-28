ce99aeea908e5e212e3a09a39829f53fc861d6e8
TobiasAagaard <tach@trifork.com>
2026-08-28 11:51:19 +0200

    Add Vehicle model

---
+++ Auction.Core/Models/Vehicle.cs
@@ -0,0 +1,40 @@
+
+namespace Auction.Core.Models;
+
+public class Vehicle
+{
+    public Vehicle(int id, string name, double kilometers, string registrationNumber, int year, bool towBar, string vehicleType, string engineType, double kmPerLiter, FuelType fuelType)
+    {
+        Id = id;
+        Name = name;
+        Kilometers = kilometers;
+        RegistrationNumber = registrationNumber;
+        Year = year;
+        TowBar = towBar;
+        VehicleType = vehicleType;
+        EngineType = engineType;
+        KmPerLiter = kmPerLiter;
+        FuelType = fuelType;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public double Kilometers { get; set; }
+    public string RegistrationNumber { get; set; } = null!;
+    public int Year { get; set; }
+    public bool TowBar { get; set; } = false;
+    public string VehicleType { get; set; } = null!;
+    public string EngineType { get; set; } = null!;
+    public double KmPerLiter { get; set; }
+    public FuelType FuelType { get; set; }
+
+}
+
+
+public enum FuelType
+{
+    Diesel,
+    Petrol,
+    Electric,
+    Hydrogen,
+}
