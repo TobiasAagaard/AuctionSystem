8763e07b82101954668094dac8e3e95865d228d3
René <reneskjodt333@gmail.com>
2026-08-28 11:30:13 +0200

    Add User class with properties and constructor

---
+++ Auction.Core/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Auction.Core.Models;
+
+public class User
+{
+
+    public User(uint id, string username, string password, uint postalCode)
+    {
+        ID = id;
+        Username = username;
+        Password = password;
+        PostalCode = postalCode;
+    }
+
+    public uint ID { get; }
+    public string Username { set; get; }
+    public string Password { set; get; }
+    public uint PostalCode { set; get; }
+}
