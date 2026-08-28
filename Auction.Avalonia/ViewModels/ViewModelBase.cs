3835ded1546bcadce7c5ff2d0a8b7071e27ef4d1
TobiasAagaard <tach@trifork.com>
2026-08-28 08:56:38 +0200

    init project structor

---
+++ Auction.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Auction.Avalonia.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Auction.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Auction.Avalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
