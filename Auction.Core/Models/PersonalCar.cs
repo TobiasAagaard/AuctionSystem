c5f43b339cb1ec5d46117c27dc9a2e9609556edb
René <reneskjodt333@gmail.com>
2026-09-04 10:28:54 +0200

    Refactor PersonalCar constructor to enforce license type validation and set default value

--- Auction.Core/Models/PersonalCar.cs
+++ Auction.Core/Models/PersonalCar.cs
@@ -5,7 +5,7 @@ namespace Auction_Core.Models;
 public abstract class PersonalCar : Vehicle
 {
 
-    public PersonalCar(
+    protected PersonalCar(
         int id,
         string name,
         double kilometers,
@@ -13,13 +13,23 @@ public abstract class PersonalCar : Vehicle
         int year,
         double basePrice,
         bool towBar,
-        LicenseType licenseType,
         double engineSize,
         double kmPerLiter,
         FuelType fuelType,
-        int seatCount
+        int seatCount,
+        LicenseType licenseType = LicenseType.B
     ) : base(id, name, kilometers, registrationNumber, year, basePrice, towBar, licenseType, engineSize, kmPerLiter, fuelType)
     {
+        if (engineSize < 0.7 || engineSize > 10.0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(engineSize), "Engine size must be between 0.7 and 10.0 liters for a personal car.");
+        }
+
+        if (licenseType is not (LicenseType.B or LicenseType.BE))
+        {
+            throw new ArgumentOutOfRangeException(nameof(licenseType), "A personal car requires either a B or BE license.");
+        }
+
         this.SeatCount = seatCount;
     }
 
