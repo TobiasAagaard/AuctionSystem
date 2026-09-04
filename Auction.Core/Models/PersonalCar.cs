f6437aa13481660c6415c790ea90ee40acaa9bf6
René <reneskjodt333@gmail.com>
2026-09-04 10:19:23 +0200

    Add PersonalCar class to represent personal vehicles in the auction system

---
+++ Auction.Core/Models/PersonalCar.cs
@@ -0,0 +1,32 @@
+﻿using Auction_Core.Enums;
+
+namespace Auction_Core.Models;
+
+public abstract class PersonalCar : Vehicle
+{
+
+    public PersonalCar(
+        int id,
+        string name,
+        double kilometers,
+        string registrationNumber,
+        int year,
+        double basePrice,
+        bool towBar,
+        LicenseType licenseType,
+        double engineSize,
+        double kmPerLiter,
+        FuelType fuelType,
+        int seatCount
+    ) : base(id, name, kilometers, registrationNumber, year, basePrice, towBar, licenseType, engineSize, kmPerLiter, fuelType)
+    {
+        this.SeatCount = seatCount;
+    }
+
+    public int SeatCount { get; set; }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, SeatCount: {SeatCount}";
+    }
+}
