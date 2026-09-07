3a78425b10796228dbcbdad41e86b08a25c4cddc
TobiasAagaard <tach@trifork.com>
2026-09-07 12:45:02 +0200

    IVehicleRepository and VehicleRepository implementations

---
+++ Auction.Core/Repository/IVehicleRepository.cs
@@ -0,0 +1,12 @@
+using Auction_Core.Models;
+
+namespace Auction_Core.Repository;
+
+public interface IVehicleRepository
+{
+   Task<IEnumerable<Vehicle>> GetAllVehiclesByIdAsync(int id);
+   Task<Vehicle>GetVehicleByIdAsync(int id);
+   Task AddVehicleAsync(Vehicle vehicle);
+   Task UpdateVehicleAsync(Vehicle vehicle);
+   Task DeleteVehicleAsync(int id);
+}
