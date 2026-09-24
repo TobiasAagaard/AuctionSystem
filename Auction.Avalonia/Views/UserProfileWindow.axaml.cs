a2bb7fcc0ec5edb35a904e85cf77e03df341151b
René <reneskjodt333@gmail.com>
2026-09-24 13:03:44 +0200

    Added window for user profile

---
+++ Auction.Avalonia/Views/UserProfileWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Auction.Avalonia.Views;
+
+public partial class UserProfileWindow : Window
+{
+    public UserProfileWindow()
+    {
+        InitializeComponent();
+    }
+}
