bdf17b79621ef23208f7b8547812ef4abc713597
René <reneskjodt333@gmail.com>
2026-09-24 12:36:48 +0200

    Added view to auction seller site

---
+++ Auction.Avalonia/Views/SellerOfAuctionWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Auction.Avalonia.Views;
+
+public partial class SellerOfAuctionWindow : Window
+{
+    public SellerOfAuctionWindow()
+    {
+        InitializeComponent();
+    }
+}
