1a27b10ea5ae845ac71d0c6c2e891a4ad467a199
TobiasAagaard <tach@trifork.com>
2026-09-25 22:57:46 +0200

    Refactor toast notification duration and update bindings in MainWindow

--- Auction.Avalonia/App.axaml.cs
+++ Auction.Avalonia/App.axaml.cs
@@ -24,7 +24,7 @@ public partial class App : Application
         var services = new ServiceCollection();
 
         services.AddCore();
-        
+
         services.AddTransient<MainViewModel>();
         services.AddTransient<LoginViewModel>();
         services.AddTransient<CreateUserViewModel>();
